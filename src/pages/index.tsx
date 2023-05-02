06ed8039dca50f0a32e5fdaef3ed91f63e053236
claud_park <yerimpark103@gmail.com>
2023-05-02 16:26:30 +0900

    Add test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello world!</main>
+      <main>Hello world!!!!!</main>
     </>
-  );
+  )
 }
