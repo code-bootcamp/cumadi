dbac4253321ced13c19cf415b2d00fc6e48cde93
claud_park <yerimpark103@gmail.com>
2023-05-03 22:52:17 +0900

    ADD-folder structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app'
 import { Global } from '@emotion/react'
 import { globalStyles } from '@/common/styles/globalStyles'
+import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Global styles={globalStyles} />
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* TODO: Add PostsList inside body/main */}
       <main>Hello world!!!!!!!!</main>
     </>
   )
