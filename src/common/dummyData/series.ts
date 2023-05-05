467139a1bcf4d2475a0ce8e3a42710ef463b16d2
dragonfruitlemonade <tpghks9245@gmail.com>
2023-05-05 14:58:03 +0900

    ADD: 시리즈 더미파일 작성

---
+++ src/common/dummyData/series.ts
@@ -0,0 +1,68 @@
+export const seriesItem = [
+  {
+    id: 'Q9vUv0E0ibc0X55kVVLr',
+    thumbnail: 'https://cdn.class101.net/images/3a25ecd9-d1ab-4d21-8cc1-522ea711e729',
+    title: '개발자 A의 고군분투',
+    description: '힘들지만 재미썽!',
+    categories: '개발',
+    tier: '주간 2위',
+    name: '개발자 A',
+    introduction: '개발세발 개발자',
+    price: 3000,
+    star: 4,
+    posts: {
+      first: {
+        categories: '개발',
+        title: '개발자로 살아남는 방법1',
+        contents: '그러게 어떻게 살아가는게 좋을까',
+        createDate: '2023.05.01',
+        image: 'https://cdn.class101.net/images/3a25ecd9-d1ab-4d21-8cc1-522ea711e729',
+      },
+      second: {
+        categories: '개발',
+        title: '개발자로 살아남는 방법2',
+        contents: '그러게 어떻게 살아가는게 좋을까',
+        createDate: '2023.05.03',
+        image: 'https://cdn.class101.net/images/3a25ecd9-d1ab-4d21-8cc1-522ea711e729',
+      },
+    },
+    review: {
+      first: {
+        name: '개발자 A',
+        star: 5,
+        contents: "와! 유익해요!",
+        createDate: '2023.05.01'
+      },
+      second: {
+        name: '개발자 B',
+        star: 4,
+        contents: "와! 너무 비싸요!",
+        createDate: '2023.05.02'
+      }
+    }
+  },
+  {
+    id: 'W9vUv0E0ibc0X55kVVLr',
+    title: '코드캠프 따라하기',
+    categories: '개발',
+    name: '코린이',
+    thumbnail: 'https://cdn.class101.net/images/3a25ecd9-d1ab-4d21-8cc1-522ea711e729',
+    star: 2,
+  },
+  {
+    id: 'E9vUv0E0ibc0X55kVVLr',
+    title: '독학개발',
+    categories: '개발',
+    name: '왕백수',
+    thumbnail: 'https://cdn.class101.net/images/3a25ecd9-d1ab-4d21-8cc1-522ea711e729',
+    star: 5,
+  },
+  {
+    id: 'R9vUv0E0ibc0X55kVVLr',
+    title: '개발자 Z의 고군분투',
+    name: 'Z',
+    categories: '개발',
+    thumbnail: 'https://cdn.class101.net/images/3a25ecd9-d1ab-4d21-8cc1-522ea711e729',
+    star: 1,
+  },
+]
