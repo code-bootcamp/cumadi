59dad239ada203790183f7c4c2a96c9546c86183
jeonpilseong <abcabc3502@gmail.com>
2023-05-15 12:47:58 +0900

    ADD:로그인 API 요청

--- src/components/features/login/login.container.tsx
+++ src/components/features/login/login.container.tsx
@@ -14,14 +14,14 @@ export default function Login() {
   // **** 상태
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  // const [, setAccessToken] = useRecoilState(accessTokenState)
+  const [, setAccessToken] = useRecoilState(accessTokenState)
 
   // ** 유효성 검사
   const [emailError, setEmailError] = useState('')
   const [passwordError, setPasswordError] = useState('')
 
   // **** PlayGround
-  // const [loginUser] = useMutation<Pick<IMutation, 'loginUser'>, IMutationLoginUserArgs>(LOGIN_USER)
+  const [loginUser] = useMutation<any>(LOGIN_USER)
 
   // **** 값이 있다면, 유효성 검사 메시지 지우기
   const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
@@ -41,29 +41,31 @@ export default function Login() {
 
   // **** 로그인 요청
   const onClickLogin = async () => {
-    //   try {
-    //     // 1. 로그인해서 accessToken 받아오기
-    //     const result = await loginUser({
-    //       variables: {
-    //         password,
-    //         email,
-    //       },
-    //     })
-    //     const accessToken = result.data?.loginUser.accessToken
-    //     // 2. accessToken을 globalState에 저장하기
-    //     if (!accessToken) {
-    //       Modal.error({ content: '로그인에 실패했습니다. 다시 시도해주세요.' })
-    //       return
-    //     }
-    //     setAccessToken(accessToken)
-    //     // 3. LocalStorage에 토큰 저장 (임시로 사용, 나중에 지울예정)
-    //     localStorage.setItem('accessToken', accessToken)
-    //     // 4. 로그인 성공 페이지로 이동하기
-    //     Modal.success({ content: '로그인 되었습니다' })
-    //     void router.push('/my/products')
-    //   } catch (error) {
-    //     if (error instanceof Error) Modal.error({ content: error.message })
-    //   }
+    try {
+      // 1. 로그인해서 accessToken 받아오기
+      const result = await loginUser({
+        variables: {
+          password,
+          email,
+        },
+      })
+
+      const accessToken = result.data?.loginUser
+
+      // 2. accessToken을 globalState에 저장하기
+      if (!accessToken) {
+        Modal.error({ content: '로그인에 실패했습니다. 다시 시도해주세요.' })
+        return
+      }
+      setAccessToken(accessToken)
+      // 3. LocalStorage에 토큰 저장 (임시로 사용, 나중에 지울예정)
+      localStorage.setItem('accessToken', accessToken)
+      // 4. 로그인 성공 페이지로 이동하기
+      Modal.success({ content: '로그인 되었습니다' })
+      void router.push('/')
+    } catch (error) {
+      if (error instanceof Error) Modal.error({ content: error.message })
+    }
   }
 
   return (
