d1d749e726f85b9f5a859cfca86817c2b4f4b0f1
dragonfruitlemonade <tpghks9245@gmail.com>
2023-05-16 14:24:00 +0900

    ADD: 시리즈 디테일 페이지 api연결 완료

--- src/components/features/series/detail/seriesDetail.container.tsx
+++ src/components/features/series/detail/seriesDetail.container.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import SeriesDetailUI from './seriesDetail.presenter'
+import { useRouter } from 'next/router'
+import { gql, useQuery } from '@apollo/client'
+
+const FETCH_SERIES = gql`
+  query fetchSeries($seriesId: String!) {
+    fetchSeries(seriesId: $seriesId) {
+      seriesId
+      title
+      introduction
+      price
+      createdAt
+      category {
+        categoryId
+        name
+      }
+      user {
+        userId
+        nickname
+      }
+    }
+  }
+`
 
 export default function SeriesDetail() {
+  const router = useRouter()
+  const seriesId = String(router.query.seriesId);
+
+  const { data } = useQuery(FETCH_SERIES, {
+    variables: { seriesId },
+  })
   return (
-    <SeriesDetailUI />
+    <SeriesDetailUI data={data}/>
   )
 }
