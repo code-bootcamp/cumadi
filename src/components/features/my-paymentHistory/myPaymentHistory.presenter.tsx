65b99b1778ade6530285721dcbf7a6512c0d052f
ParkSohyunee <sohyunee016@gmail.com>
2023-05-17 09:55:21 +0900

    ADD: 구매내역 페이지 UI 구현

--- src/components/features/my-paymentHistory/myPaymentHistory.presenter.tsx
+++ src/components/features/my-paymentHistory/myPaymentHistory.presenter.tsx
@@ -5,6 +5,7 @@ import {
   FlexColumnContainer,
   HorizontalCardSm,
   InfoSectionContainer,
+  MyButton,
   PriceContainer,
 } from '@/components/common/customComponent.styles'
 import * as S from './myPaymentHistory.styles'
@@ -15,24 +16,27 @@ export default function MyPaymentHistoryUI() {
 
   return (
     <S.Body>
-      {postItem.map(el => (
-        <HorizontalCardSm>
-          <img className="horizontal-card-cover" alt="example" src={el.image} />
-          <div className="horizontal-card-body" style={{ display: 'flex', alignItems: 'center' }}>
-            <FlexColumnContainer gap={'0.5rem'}>
-              <BodyTextLg>{el.title}</BodyTextLg>
-              <InfoSectionContainer>
-                <BodyTextSm color={Colors.gray1}>주문날짜</BodyTextSm>
-              </InfoSectionContainer>
-            </FlexColumnContainer>
-            <PriceContainer>
-              <BodyTextLg>3,000원</BodyTextLg>
-            </PriceContainer>
-            <BodyTextLg>환불 신청</BodyTextLg>
-            <BodyTextLg>리뷰 작성</BodyTextLg>
-          </div>
-        </HorizontalCardSm>
-      ))}
+      <S.CheckListWrapper>
+        {postItem.map(el => (
+          <S.CardWrapper key={el.id} className="card-wrapper">
+            <HorizontalCardSm>
+              <img className="horizontal-card-cover" alt={`${el.title} 이미지`} src={el.image} />
+              <div className="horizontal-card-body" style={{ display: 'flex', alignItems: 'center' }}>
+                <FlexColumnContainer gap={'0.5rem'}>
+                  <BodyTextLg>{el.title}</BodyTextLg>
+                  <InfoSectionContainer>
+                    <BodyTextSm color={Colors.gray1}>구매날짜</BodyTextSm>
+                  </InfoSectionContainer>
+                </FlexColumnContainer>
+                <PriceContainer>
+                  <BodyTextLg>{`${el.price.toLocaleString()}원`}</BodyTextLg>
+                </PriceContainer>
+              </div>
+            </HorizontalCardSm>
+            <MyButton>리뷰작성</MyButton>
+          </S.CardWrapper>
+        ))}
+      </S.CheckListWrapper>
     </S.Body>
   )
 }
--- src/components/features/my-paymentHistory/myPaymentHistory.styles.ts
+++ src/components/features/my-paymentHistory/myPaymentHistory.styles.ts
@@ -1,15 +1,38 @@
 import { breakPoints } from '@/common/styles/media'
 import styled from '@emotion/styled'
 
-// **** 아바타 상단
 export const Body = styled.section`
+  display: inline-flex;
+  flex-direction: column;
   width: 83rem;
+  padding: 1rem;
+  /* background-color: yellowgreen; */
 
   @media ${breakPoints.tablet} {
-    width: 55rem;
+    width: 60rem;
   }
 
   @media ${breakPoints.mobile} {
-    width: 30rem;
+    width: 35rem;
+
+    .card-wrapper .horizontal-card-cover {
+      width: 10rem;
+    }
+    .card-wrapper .horizontal-card-body {
+      padding: 0.5rem;
+    }
   }
+`
+
+// 나중에 적용!!!
+export const CheckListWrapper = styled.div`
+  /* height: 52rem; */
+`
+
+export const CardWrapper = styled.div`
+  display: inline-flex;
+  gap: 1rem;
+  width: 100%;
+  margin: 0.5rem 0;
+  align-items: center;
 `
