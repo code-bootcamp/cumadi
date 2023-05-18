98bf8f452a8d219f15f95909f5759e078369d372
ParkSohyunee <sohyunee016@gmail.com>
2023-05-18 13:32:19 +0900

    ADD: 결제로직 수정, types 파일 업데이트

--- src/common/libraries/payment.ts
+++ src/common/libraries/payment.ts
@@ -2,47 +2,71 @@ declare const window: typeof globalThis & {
   IMP: any
 }
 
-export const purchaseProduct = (product: any, price: number, router: any) => {
-  // console.log(product); // [{…}, {…}, {…}, ...]
-  const carts = JSON.parse(localStorage.getItem('carts') || '[]')
-  console.log('삭제 전 장바구니: ', carts)
+export const purchaseProduct = (
+  product: any,
+  price: number,
+  router: any,
+  createPaymentSeries: any,
+  seriesId: string,
+) => {
+  console.log(product) // [{…}, {…}, {…}, ...]
+  console.log(seriesId)
+
+  // const carts = JSON.parse(localStorage.getItem('carts') || '[]')
+  // console.log('삭제 전 장바구니: ', carts)
+  console.log(price)
 
   const IMP = window.IMP
-  IMP.init('imp06164883') // 코드캠프 계정으로 변경 예정
+  IMP.init('imp71265174') // 나의 식별코드 imp06164883 imp71265174 imp49910675
 
-  // 결제창 호출
+  // 결제창 호출 imp_685546850317
   IMP.request_pay(
     {
-      pg: 'nice',
+      pg: 'nice.nictest04m',
       pay_method: 'card',
-      name: `시리즈 ${product.length}개 구매`,
+      name: `시리즈 구매`,
       amount: price,
       buyer_email: 'gildong@gmail.com', // 유저 아이디(=메일)
       buyer_name: '홍길동', // 유저 이름
       m_redirect_url: `http://localhost:3000${router.pathname}`, // 모바일에서는 결제시, 결제페이지로 사이트가 이동되므로 돌아올시 주소입력(바로구매 페이지 또는 장바구니 페이지)
     },
-    (rsp: any) => {
+    async (rsp: any) => {
+      console.log('열리나')
       if (rsp.success) {
+        console.log('열렸나')
         // console.log(product);
 
         // 결제 성공 시 로직,
         // 백엔드에 결제관련 데이터 넘겨주기  =>  API
 
-        // console.log(rsp);  // Object {imp_uid: ""}
+        // ApolloError: Variable "$createPaymentInput" of required type "CreatePaymentInput!" was not provided.
+        console.log(rsp) // Object {imp_uid: ""}
+
+        const result = await createPaymentSeries({
+          variables: {
+            createPaymentInput: {
+              impUid: rsp.imp_uid,
+              amount: 100,
+              seriesList: ['622d9455-16f8-4ea5-91e5-c30bc488a591'],
+            },
+          },
+        })
+
+        console.log(result)
 
         // 백엔드에서 받아온 ENUM 값인 status가 "성공"이면  =>  장바구니에서 결제한 상품을 제외하여 배열로 담기
-        const result = carts.filter((item: any) => {
-          return !product.some((el: any) => el.id === item.id)
-        })
-        console.log('결제 제외 상품 : ', result)
+        // const result = carts.filter((item: any) => {
+        //   return !product.some((el: any) => el.id === item.id)
+        // })
+        // console.log('결제 제외 상품 : ', result)
 
         //  결제하지 않은 상품 localStorage에 저장
-        localStorage.setItem('carts', JSON.stringify(result))
+        // localStorage.setItem('carts', JSON.stringify(result))
 
         //  장바구니의 모든 아이템을 구매했을 때는 localStorage 비워주기
-        if (localStorage.getItem('carts') === '[]') {
-          localStorage.removeItem('carts')
-        }
+        // if (localStorage.getItem('carts') === '[]') {
+        //   localStorage.removeItem('carts')
+        // }
         alert('결제에 성공했습니다.')
         router.push('/my/paymentHistory')
 
@@ -50,6 +74,7 @@ export const purchaseProduct = (product: any, price: number, router: any) => {
         //
       } else {
         // 결제 취소 시 로직,
+        console.log('닫히나')
         alert('결제가 종료되었습니다. 다시 시도해 주세요.')
       }
     },
