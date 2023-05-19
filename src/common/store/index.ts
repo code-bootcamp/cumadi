d56d3e6bc128e560959b1fd3066a2c5f53c60934
Beren <berenickt99@gmail.com>
2023-05-19 10:32:11 +0900

    FIX: getAccessToken-Duplicate atom key

--- src/common/store/index.ts
+++ src/common/store/index.ts
@@ -1,7 +1,6 @@
 import { tags } from './../dummyData/tags'
 import { atom, selector } from 'recoil'
 import { v1 } from 'uuid'
-import { getAccessToken } from '../libraries/getAccessToken'
 
 // ** cf. https://velog.io/@sj_dev_js/Recoil-Duplicate-atom-key
 // [에러 해결] Recoil : Duplicate atom key
@@ -37,10 +36,10 @@ export const tagsState = atom({
 })
 
 export const restoreAccessTokenLoadable = selector({
-  key: 'restoreAccessTokenLoadable',
+  key: `restoreAccessTokenLoadable/${v1()}`,
   get: async () => {
-    const newAccessToken = await getAccessToken()
-    return newAccessToken
+    // const newAccessToken = await getAccessToken()
+    // return newAccessToken
   },
 })
 
