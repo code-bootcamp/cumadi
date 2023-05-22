8bf4f33bde8c164457e56a72974115339fddfc7a
claud_park <yerimpark103@gmail.com>
2023-05-22 20:55:41 +0900

    FIX-Add new server url

--- src/common/libraries/getAccessToken.ts
+++ src/common/libraries/getAccessToken.ts
@@ -9,7 +9,7 @@
 
 // export const getAccessToken = async (): Promise<string | undefined> => {
 //   try {
-//     const graphQLClient = new GraphQLClient('http://34.64.43.6:3000/graphql', { credentials: 'include' })
+//     const graphQLClient = new GraphQLClient('http://34.64.249.172:3000/graphql', { credentials: 'include' })
 //     const result = await graphQLClient.request<any>(RESTORE_ACCESS_TOKEN)
 //     const newAccessToken = result.restoreAccessToken
 //     return newAccessToken
