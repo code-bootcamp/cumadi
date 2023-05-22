846261aee5e1165a72e3a2e1b9d9b9b08d0b4a1c
dragonfruitlemonade <tpghks9245@gmail.com>
2023-05-22 14:31:57 +0900

    ADD: 시리즈 리뷰 리스트 컨테이너

--- src/components/features/series-answer/list/seriesAnswerList.container.tsx
+++ src/components/features/series-answer/list/seriesAnswerList.container.tsx
@@ -1,5 +1,15 @@
+import { useRouter } from 'next/router'
 import SeriesAnswerListUI from './seriesAnswerList.presenter'
+import { useQuery } from '@apollo/client'
+import { FETCH_SERIES_REVIEWS_BY_SERIES } from './seriesAnswerList.query'
 
 export default function SeriesAnswerList() {
-  return <SeriesAnswerListUI />
+  const router = useRouter()
+  const seriesId = String(router.query.seriesId)
+
+  const { data } = useQuery(FETCH_SERIES_REVIEWS_BY_SERIES, {
+    variables: { seriesId }
+  })
+
+  return <SeriesAnswerListUI data={data} />
 }
