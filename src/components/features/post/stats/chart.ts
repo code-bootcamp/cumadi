6196234330bbb7434554ae2b99e48d93a7c7d520
Beren <berenickt99@gmail.com>
2023-05-22 15:51:10 +0900

    ADD: 포스트-조회수-정렬해서

--- src/components/features/post/stats/chart.ts
+++ src/components/features/post/stats/chart.ts
@@ -1,6 +1,6 @@
 export const options = {
   // 옵션 (1) : 반응형 여부
-  // responsive: true,
+  responsive: true,
   // 옵션 (2) : 차트 데이터 각각의 지점에 커서를 올리면 뜨는 것
   interaction: {
     mode: 'index' as const,
@@ -22,20 +22,3 @@ export const options = {
     },
   },
 }
-
-// **** 차트에 들어갈 데이터를 담고 있는 객체
-// export const data = {
-//   // labels가 y축 index
-//   labels: ['0', '5', '9', '13', '17', '21', '25'],
-//   // 데이터 관련 정보(값, 컬러, 라벨 등)을 담은 dataset 객체들을 가지고 있는 배열
-//   datasets: [
-//     {
-//       label: '일일 방문자수',
-//       borderColor: 'rgb(53, 162, 235)',
-//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-//       borderWidth: 1,
-//       data: [128, 119, 13, 15, 12, 13, 8],
-//       // data: newArr,
-//     },
-//   ],
-// }
--- src/components/features/post/stats/postStats.presenter.tsx
+++ src/components/features/post/stats/postStats.presenter.tsx
@@ -15,59 +15,53 @@ import * as S from './postStats.styles'
 import { IPostStatsUIProps } from './postStats.types'
 import { getDate, getToday } from '@/common/libraries/utils'
 import { options } from './chart'
+import { IStatistics } from '@/common/types/generated/types'
 
 // ** cf. https://velog.io/@treejy/React%EC%97%90%EC%84%9C-Chart.js-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0-with-TypeScript#%EC%84%A4%EC%B9%98%ED%95%98%EA%B8%B0
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
-const newArr: number[] = []
-
-const data = {
-  // labels가 y축 index
-  // prettier-ignore
-  labels: [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
-    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', 
-    '21', '22', '23', '24', '26', '27', '28', '29', '30', '31',
-  ],
-  // 데이터 관련 정보(값, 컬러, 라벨 등)을 담은 dataset 객체들을 가지고 있는 배열
-  datasets: [
-    {
-      label: '일일 방문자수',
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      borderWidth: 1,
-      // data: [128, 119, 13, 15, 12, 13, 8],
-      data: newArr,
-    },
-  ],
-}
+const ObjPostView: any = {}
 
 export default function PostStatsUI({ stateData }: IPostStatsUIProps) {
   const StateData = stateData?.fetchPostViewOfMine
 
-  let sumView = 0
-  let todayView = 0
-
-  // console.log('StateData==========')
-  // console.log(StateData)
-
-  const sortStaetArr = [...StateData]
-  sortStaetArr.sort((a, b) => new Date(b.date) - new Date(a.date))
-  console.log('sortStaetArr==========')
-  console.log(sortStaetArr)
-
-  StateData?.map(day => (sumView += day.view))
+  // **** 데이터에서 전체-일일 방문자 수를 계산해 보여줌
+  let totalNumberViewsOfPost = 0
+  let dayNumberViewsOfPost = 0
+  StateData?.map(day => (totalNumberViewsOfPost += day.view))
   StateData?.map(day => {
-    if (getDate(day.date) === getToday()) todayView += day.view
+    if (getDate(day.date) === getToday()) dayNumberViewsOfPost += day.view
   })
-  StateData?.map(day => newArr.unshift(day.view))
-  // console.log(newArr)
+
+  // **** 날짜별로 정렬
+  if (StateData !== undefined) {
+    const sortStaetArr = [...StateData]
+    sortStaetArr.sort((a, b) => {
+      if (getDate(a.date) < getDate(b.date)) return -1
+      if (getDate(a.date) > getDate(b.date)) return 1
+      return 0
+    })
+
+    // ** 키(날짜) 값(일일조회수)으로 객체 만들기
+    let keys = sortStaetArr?.map(item => getDate(item.date))
+    let values = sortStaetArr?.map(item => item.view)
+
+    keys.forEach((key, i) => (ObjPostView[key] = values[i]))
+  }
+
+  // **** 키(날짜) 값(일일조회수)으로 객체 만들기
+  // if (StateData !== undefined) {
+  //   let keys = StateData?.map(item => getDate(item.date))
+  //   let values = StateData?.map(item => item.view)
+
+  //   keys.forEach((key, i) => (ObjPostView[key] = values[i]))
+  // }
 
   return (
     <S.Container>
       <S.TopBox>
-        <S.VisitorNumber>전체 방문자 수: {sumView}</S.VisitorNumber>
-        <S.VisitorNumber>일일 방문자 수: {todayView}</S.VisitorNumber>
+        <S.VisitorNumber>전체 방문자 수: {totalNumberViewsOfPost}</S.VisitorNumber>
+        <S.VisitorNumber>일일 방문자 수: {dayNumberViewsOfPost}</S.VisitorNumber>
       </S.TopBox>
       {/* 메인 통계 보여줄 곳 */}
       <S.ChartWrapper>
@@ -75,4 +69,19 @@ export default function PostStatsUI({ stateData }: IPostStatsUIProps) {
       </S.ChartWrapper>
     </S.Container>
   )
+}
+
+// **** 차트에 들어갈 데이터를 담고 있는 객체
+const data: any = {
+  // 데이터 관련 정보(값, 컬러, 라벨 등)을 담은 dataset 객체들을 가지고 있는 배열
+  datasets: [
+    {
+      type: 'line',
+      label: '일일 방문자수',
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      borderWidth: 1,
+      data: ObjPostView,
+    },
+  ],
 }
